ac3507ab162c269cd27458e8ee4979caadd13e71
Wolfram Kriesing <wk@uxebu.com>
2014-09-04 14:20:54 +0200

    Show models in admin.

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Session
+from models import Spec
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'started_at']
+
+class SpecAdmin(admin.ModelAdmin):
+    list_display = ['code', 'session', 'author', 'saved_at']
+
+admin.site.register(Session, SessionAdmin)
+admin.site.register(Spec, SpecAdmin)
