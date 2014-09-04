51660291b043b88eab599c59d8c1ef7ae9dc74d7
Wolfram Kriesing <wk@uxebu.com>
2014-09-04 14:21:06 +0200

    Use the existing default name.

--- src/core/models.py
+++ src/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from util.session import get_or_generate_session_name
+from util.session import DEFAULT_SESSION_NAME_PREFIX
 
 class Session(models.Model):
     name = models.CharField(max_length=255)
@@ -13,7 +14,7 @@ class Session(models.Model):
         return self.name
 
     def save(self, *args, **kwargs):
-        existing_session_names = Session.objects.filter(name__startswith=self.UNTITLED_PREFIX, user=self.user).only('name')
+        existing_session_names = Session.objects.filter(name__startswith=DEFAULT_SESSION_NAME_PREFIX, user=self.user).only('name')
         self.name = get_or_generate_session_name(self.name, existing_session_names)
         super(Session, self).save(*args, **kwargs) # Call the "real" save() method.
 
