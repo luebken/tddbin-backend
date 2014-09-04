703f4fcba5eae2e99a57f2859a0b5a38d5d7428c
Wolfram Kriesing <wk@uxebu.com>
2014-09-04 11:58:46 +0200

    Move production code into it's own file.

--- src/core/tests.py
+++ src/core/tests.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 
-DEFAULT_SESSION_NAME_PREFIX = 'Untitled '
-
-def generate_session_name(existing_names):
-    if len(existing_names) == 0:
-        return DEFAULT_SESSION_NAME_PREFIX + '1'
-
-    used_postfixes = []
-    for name in existing_names:
-        name_left_over = name.replace(DEFAULT_SESSION_NAME_PREFIX, '')
-        if name_left_over.isdigit():
-            used_postfixes.append(int(name_left_over))
-    used_postfixes.sort()
-    new_postfix = used_postfixes[-1] + 1
-    return DEFAULT_SESSION_NAME_PREFIX + str(new_postfix)
-
-def get_session_name(name, existing_names):
-    if name:
-        return name
-    return generate_session_name(existing_names)
+from util.session import DEFAULT_SESSION_NAME_PREFIX
+from util.session import get_session_name
 
 class UntitledSessionNamesTests(TestCase):
     def test_get_first_session_name(self):
---
+++ src/core/util/session.py
@@ -0,0 +1,20 @@
+DEFAULT_SESSION_NAME_PREFIX = 'Untitled '
+
+def generate_session_name(existing_names):
+    if len(existing_names) == 0:
+        return DEFAULT_SESSION_NAME_PREFIX + '1'
+
+    used_postfixes = []
+    for name in existing_names:
+        name_left_over = name.replace(DEFAULT_SESSION_NAME_PREFIX, '')
+        if name_left_over.isdigit():
+            used_postfixes.append(int(name_left_over))
+    used_postfixes.sort()
+    new_postfix = used_postfixes[-1] + 1
+    return DEFAULT_SESSION_NAME_PREFIX + str(new_postfix)
+
+def get_session_name(name, existing_names):
+    if name:
+        return name
+    return generate_session_name(existing_names)
+
